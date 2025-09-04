b4093f3e850db36860b509bb89675e4d589589ed
casti <felipecastilhos@gmail.com>
2025-09-04 14:11:24 -0300

    feat: add obsidian sync systemd timer and service

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -6,16 +6,22 @@ alias ls="lsd --color=auto"
 alias android-studio="~/Library/android-studio/bin/studio.sh"
 alias spritesheetify="~/scripts/spritesheetify.sh"
 alias bookr="~/scripts/tools/books/book_rename.sh"
-alias jupyverse="$MINICONDA/jupyverse"
 alias mongodb="/usr/bin/mongod"
 alias cd="z"
 alias vim="nvim"
 alias cat="bat -pp"
+
+#python - there are some functions defined in functions.zsh
+alias jupyverse="$MINICONDA/jupyverse"
 
 # dotfiles utilities
 alias dotbkper="~/dotfiles/scripts/dotbkper.sh"
 alias dotsync="~/dotfiles/scripts/dotsync.sh"
 alias dotstatus="~/dotfiles/scripts/dotstatus.sh"
 alias dotlink="~/dotfiles/scripts/dotlink.sh"
+
+#image
 alias imgcat="wezterm imgcat"
 
+#obsidian
+alias obsidian-sync="$HOME/scripts/tools/obsidian/obsidian-sync.zsh"
